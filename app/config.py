1589b596823349ce13dcf039937c55c7b8ef2e63
AdrianD <Adrian.Domagala71@gmail.com>
2025-10-04 22:13:24 +0200

    test

--- app/config.py
+++ app/config.py
@@ -1,3 +1,4 @@
+# app/config.py
 import os
 from pydantic_settings import BaseSettings
 
@@ -18,4 +19,6 @@ class Settings(BaseSettings):
         env_file_encoding = 'utf-8'
         extra = "ignore"
 
-settings = Settings()
+SUPABASE_URL = os.environ.get("SUPABASE_URL") or os.environ.get("SB_URL")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY") or os.environ.get("SB_API")
+SB_PASSWORD = os.environ.get("SB_PASSWORD")
